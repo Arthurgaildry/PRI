9651f0681bdb51485df777bd6bbfd95ad91aaca0
Arthurgaildry <arthur2_917@hotmail.fr>
2017-11-28 17:48:09 +0100

    Client_Serveur

--- README.md
+++
@@ -1,8 +0,0 @@
-# PRI - Projet Recherche et Innovation
-
- > Arthur Gaildry - David Moulin 
- 
- 
-## About the project
- 
-TBD
---
+++ serveur/FenServeur.h
@@ -0,0 +1,33 @@
+#ifndef HEADER_FENSERVEUR
+#define HEADER_FENSERVEUR
+
+#include <QtGui>
+#include <QtNetwork>
+#include<Qwidget>
+#include<Qtwidgets>
+
+
+
+class FenServeur : public QWidget
+{
+    Q_OBJECT
+
+    public:
+        FenServeur();
+        void envoyerATous(const QString &message);
+
+    private slots:
+        void nouvelleConnexion();
+        void donneesRecues();
+        void deconnexionClient();
+
+    private:
+        QLabel *etatServeur;
+        QPushButton *boutonQuitter;
+
+        QTcpServer *serveur;
+        QList<QTcpSocket *> clients;
+        quint16 tailleMessage;
+};
+
+#endif
