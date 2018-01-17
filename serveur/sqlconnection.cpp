ce4d2b5dba80b2b2cfff4d826b51ea0a4336af1c
Arthurgaildry <arthur2_917@hotmail.fr>
2018-01-17 14:58:31 +0100

    merge de tout
    merger

--- serveur/sqlconnection.cpp
+++ serveur/sqlconnection.cpp
@@ -9,7 +9,7 @@ SqlConnection::SqlConnection()
 }
 
 
-QString SqlConnection::connection(QString id){
+QString SqlConnection::connection(QString id){// id---> mess (mess0/mess1)
     QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
     db.setHostName("localhost");
     db.setDatabaseName("bdd_pri");
@@ -23,19 +23,63 @@ QString SqlConnection::connection(QString id){
     {
          //QMessageBox::critical(this, "Erreur", "Erreur de connexion à la bdd.");
     } else {
-        QSqlQuery query("SELECT * FROM clients where id= ? ");
-        query.addBindValue(id.toInt());
+        QSqlQuery query("SELECT * FROM clients where id= ? ");//id--> mess1  Ici associer la clé à l'id pour identifier les données à afficher.
+        query.addBindValue(id.toInt());//Transformation d'id en int pour l'utiliser dans le programme ,!!!! verifier si int marche avec clé binaire
         if (! query.exec()){
              return QString(); // erreur
         }
 
         if(query.first()){
-              while(i<8){
+              while(i<7){//  Recuperation des 7 lignes de la table client
                 rec.append(query.value(i).toString());
                 rec.append("  ");
                 i=i+1;
 
+
+
+
             }
+              /*
+                              int A[2]; definition de tableau pour ressortir les données souhaitées
+                              int M[6];
+                              int P[4];
+                              A[0]=1;
+                              A[1]=2;
+                              A[2]=5;
+                              M[0]=0;
+                              M[1]=1;
+                              M[2]=2;
+                              M[3]=3;
+                              M[4]=4;
+                              M[5]=5;
+                              M[6]=7;
+                              P[0]=1;
+                              P[1]=2;
+                              P[2]=4;
+                              P[3]=5;
+                              P[4]=6;
+                              if(mess0=medecin){
+                                for(int i=0; i<6; i++){
+                                    rec.append(query.value(M[i]).toString());
+                                    rec.append("  ");
+                                }
+                              }
+                                if(mess0=police){
+                                     for(int i=0; i<4; i++){
+                                         rec.append(query.value(P[i]).toString());
+                                         rec.append("  ");
+                                     }
+                                }
+                                if(mess0=autre){
+                                     for(int i=0; i<2; i++){
+                                         rec.append(query.value(A[i]).toString());
+                                         rec.append("  ");
+                                     }
+                                }
+                             Fin du codage pour les differents cas
+                              */
+
+
 
         //else{
            // while(i<2){
@@ -49,3 +93,4 @@ QString SqlConnection::connection(QString id){
 
     }
 }
+
